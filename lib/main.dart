e2fb26855b149958f9176fb90e3dbe19d19d6568
SureshSonar <mandvekarsamadhan@gmail.com>
2026-06-06 21:28:30 +0530

    assignment 8 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:assignment/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,40 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Assignment5(),
+      home: HomeScreen(),
     );
   }
 }
-class Assignment5 extends StatelessWidget {
-  const Assignment5({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Assignement 5"),),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: 10,left: 20,right: 20),
-            padding: EdgeInsets.all( 16),
-            decoration: BoxDecoration(
-              color: Colors.purple.shade50,
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.person,color: Colors.purple,size: 16,),
-                Text("Suresh Mandvekar",style: TextStyle(color: Colors.purple.shade400,fontSize: 16),),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
